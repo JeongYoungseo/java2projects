e5218c945f30c517c2425d47fc26aceabe34c1cf
JeongYoungseo <wjddudtjjys@gmail.com>
2025-09-30 13:43:59 +0900

    Decoding

--- src/w0924/DecodingSecureFileTest.java
+++ src/w0924/DecodingSecureFileTest.java
@@ -1,25 +1,41 @@
 package w0924;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 
 public class DecodingSecureFileTest {
     public static void main(String[] args) {
         FileReader fr = null;
         BufferedReader br = null;
+        FileWriter fw = null;
 
         try {
             fr = new FileReader("D:/filetest/secure1.txt");
             br = new BufferedReader(fr);
+            fw = new FileWriter("D:/filetest/decode1.txt");
+            String line;
+            while ((line = br.readLine()) != null) {
+                String decodeStr = "";
+                for (int i = 0; i < line.length(); i++) {
+                    int code = (int)line.charAt(i);
+                    code -= 100;
+                    decodeStr += (char)code;
+                }
+                System.out.println(decodeStr);
+                fw.write(decodeStr+"\n");
+            }
         } catch (FileNotFoundException e) {
-            System.out.println("File Read Error");
-        }
-        try {
-            fr.close();
+            System.out.println("File Found Error");
         } catch (IOException e) {
-            System.out.println("Close Error");
+            System.out.println("File Read Error");
+        } finally {
+            try {
+                if(br != null) br.close();
+                if(fr != null) fr.close();
+                if(fw != null) fw.close();
+            } catch (IOException e) {
+                System.out.println("Close Error");
+            }
+
         }
     }
 

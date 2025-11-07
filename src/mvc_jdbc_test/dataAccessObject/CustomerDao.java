14eeee04a2bb161e6b9d934e7818c4b5c57ed452
JeongYoungseo <wjddudtjjys@gmail.com>
2025-11-07 13:46:13 +0900

    데이터베이스 실무 중간고사 과제(MainController,CustomerDao)

--- src/mvc_jdbc_test/dataAccessObject/CustomerDao.java
+++ src/mvc_jdbc_test/dataAccessObject/CustomerDao.java
@@ -4,7 +4,16 @@ import java.sql.*;
 
 import mvc_jdbc_test.entity.Customer;
 
-//Data Access Object ==> DB 접근 전용 클래스(데이터베이스와 직접 통신하는 역할)
+// Data Access Object (DAO)
+// ==> DB 접근 전용 클래스 (데이터베이스와 직접 통신하는 역할)
+// ==> 데이터베이스와 직접 연결되어 데이터를 읽고, 쓰고, 수정하고, 삭제하는 기능 수행
+// ==> 그 중 '수정(Update)'과 '삭제(Delete)' 기능을 구현함
+// [DAO 사용 이유]
+// 1. MainController의 코드가 간결해지고 가독성이 높아짐
+// 2. DB 코드를 직접 다루면 복잡해져 유지보수가 어려워지므로 분리함
+// 3. 데이터베이스 구조가 변경되어도 DAO 파일만 수정하면 되어 유지보수와 테스트가 편리함
+// 4. 각 클래스의 역할이 명확히 구분되어, 오류 발생 시 해당 부분만 수정하면 됨
+// 5. 실제 DB 처리는 DAO가 담당하여 구조적으로 안정적임
 public class CustomerDao {
     public Customer getCustomerId (Connection conn, String id) {
         Customer c = null;
